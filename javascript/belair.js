5564a42245ea89f1301a58741b31e97c7eb48619
James Snell <Vulpes3.1415@gmail.com>
2021-02-25 22:25:45 -0700

    Refined ideas

---
+++ javascript/belair.js
@@ -0,0 +1,10 @@
+roughDriversOne = img src="assets/images/2-1.jpeg" alt="Drivers side one/>;
+roughDriversTwo = img src="assets/images/2-2.jpeg" alt="Drivers side two/>
+
+roughCar = [
+
+
+    "<img src="/project-one/assets/images/2-5.jpeg" alt="Drivers side three/>",
+    
+
+]
