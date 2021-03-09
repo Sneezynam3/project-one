82c6bb374bb672ac5e1b60b24dd356f13da7fe5f
James Snell <Vulpes3.1415@gmail.com>
2021-03-08 17:21:19 -0700

    polishing

--- javascript/belair.js
+++ javascript/belair.js
@@ -1,3 +1,21 @@
+
+
+//the vision is placing images in an array then delaying the time of which they are displayed.
+
+
+var roughCarDriver = [
+    "2-1.jpeg",
+    "2-2.jpeg",
+    "2-5.jpeg"
+];
+
+var roughCarPassenger = [
+    "3-0.jpeg",
+    "3-4.jpeg",
+    "3-7.jpeg",
+];
+
+
 var gif;
 
 function display(){
@@ -7,21 +25,3 @@ function display(){
 function newfunction() {
     stuff and things with timages
 }
-
-
-
-
-
-
-roughCarPassenger = [
-    "3-0.jpeg",
-    "3-4.jpeg",
-    "3-7.jpeg",
-]
-
-
-roughCarDriver = [
-    "2-1.jpeg",
-    "2-2.jpeg",
-    "2-5.jpeg"
-]
