f19cd90b60cdd8b6a2f08abebecaf6bad1e7285b
James Snell <Vulpes3.1415@gmail.com>
2021-04-29 17:50:30 -0600

    Added attribute selector for a href tag, downsized images

--- javascript/belair.js
+++ javascript/belair.js
@@ -2,24 +2,7 @@
 
 //the vision is placing images in an array, then calling them with a delay to the DOM
 
-var roughCarDriver = [
-    "2-1.jpeg",
-    "2-2.jpeg",
-    "2-5.jpeg"
-];
 
-var roughCarPassenger = [
-    "3-0.jpeg",
-    "3-4.jpeg",
-    "3-7.jpeg",
-];
-
-
-var gif;
-
-function display(){
-    gif = setInterval(newfunction, 1000);
-}
 
 /*function newfunction() {
     stuff and things with timages
