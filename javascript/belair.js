b0f1551742694535a0315ff34d7346eefa6dccda
James Snell <Vulpes3.1415@gmail.com>
2021-06-01 16:44:50 -0600

    Starting JavaScript

--- javascript/belair.js
+++ javascript/belair.js
@@ -7,3 +7,17 @@
 /*function newfunction() {
     stuff and things with timages
 }*/
+
+
+var roughCar = ["../project-one/assets/images/2-1copy.jpeg", "../project-one/assets/images/2-2copy.jpeg", "../project-one/assets/images/2-5copy.jpeg"]
+
+while(i<10) {
+    carLoop(i)
+    loopCounter++;
+}
+
+function carLoop() {
+    roughCar
+    ;
+}
+
