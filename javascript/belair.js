adb16367a79151cd6e6c1e18b0f8a5a6e2d8ec48
James Snell <Vulpes3.1415@gmail.com>
2021-06-05 13:29:21 -0600

    downsized images uploaded to dreamhost

--- javascript/belair.js
+++ javascript/belair.js
@@ -15,10 +15,40 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 500); // Change image every 2 seconds
+  setTimeout(showSlides, 1000); // Change image every 2 seconds
 }
 
 
 
 
 
+var slideshowIndex = 1;
+showSlidesShow(slideshowIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlidesShow(slideshowIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlidesShow(slideshowIndex = n);
+}
+
+function showSlidesShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideshowIndex = 1}
+  if (n < 1) {slideshowIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideshowIndex-1].style.display = "block";
+  dots[slideshowIndex-1].className += " active";
+  captionText.innerHTML = dots[slideshowIndex-1].alt;
+}
